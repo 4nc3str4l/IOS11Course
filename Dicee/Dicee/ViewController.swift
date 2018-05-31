575663fe0fe4184cfbf80fc63765a985d0c642cc
Cristian Muriel Ordoñez <cristianmurielordonez@iMac-de-Cristian.local>
2018-05-31 23:13:13 +0200

    Refactor a little bit by creating some functions

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,31 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var randomDiceIndex1 : UInt32 = 0
-    var randomDiceIndex2 : UInt32 = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        randomDiceIndex1 = arc4random_uniform(6)
-        randomDiceIndex2 = arc4random_uniform(6)
-        
-        print(randomDiceIndex1)
-        
+    
+    func getRandomDice(element: UIImageView){
+        element.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
     }
     
+    func updateDiceImages(){
+        getRandomDice(element: diceImageView1)
+        getRandomDice(element: diceImageView2)
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
 }
 
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 25
+
+myAge += 1
+
+let myName : String = "4nc3str4l"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullname = myName + " R3c4ll"
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+let text: String = "abc"
+let booleans : Bool = true
+let floatingPointNumber : Float = 1.3
+let double: Double = 3.141592265359
+
+
+
+
+
