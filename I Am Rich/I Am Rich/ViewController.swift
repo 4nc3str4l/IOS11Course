23ac50dc63e3fd26178f07fff375d114ab3f0406
Cristian Muriel Ordoñez <cristianmurielordonez@iMac-de-Cristian.local>
2018-05-31 08:03:35 +0200

    First App

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Cristian Muriel Ordoñez on 31/5/18.
+//  Copyright © 2018 Cristian Muriel Ordoñez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
