b94e9b2f52c05349890765b7de0abbaa2e3e07df
4nc3str4l <cmuri_10@hotmail.com>
2018-06-16 10:48:39 +0200

    Stack viewStack views

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Cristian Muriel Ordoñez on 16/6/18.
+//  Copyright © 2018 Cristian Muriel Ordoñez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
