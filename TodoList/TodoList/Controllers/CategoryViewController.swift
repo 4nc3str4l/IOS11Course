8890c77e8227b4165711c86d649fe11fa574581f
4nc3str4l <cmuri_10@hotmail.com>
2018-06-18 18:21:43 +0200

    Core data example complete

--- TodoList/TodoList/Controllers/CategoryViewController.swift
+++ TodoList/TodoList/Controllers/CategoryViewController.swift
@@ -32,7 +32,14 @@ class CategoryViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
     }
 
     //MARK: - Add new categories
