958d1e82de7bfa95256050126c6d6f2579a181c3
4nc3str4l <cmuri_10@hotmail.com>
2018-06-18 23:53:54 +0200

    Table height

--- TodoList/TodoList/Controllers/CategoryViewController.swift
+++ TodoList/TodoList/Controllers/CategoryViewController.swift
@@ -18,8 +18,7 @@ class CategoryViewController: SwipeTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategories()
-        
-        tableView.rowHeight = 80.0
+
     }
 
     //MARK: - TableView Datasource Methods
